749a53a56e44864e95ba9e840b271e7a740ff3b3
ciane-pereira <ciciap@terra.com.br>
2022-12-02 01:02:57 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":"1",       
+        "title":"Saiu novo filme",
+        "description": "Excelente filme",
+        "photoCover":"https://st4.depositphotos.com/4634235/28796/i/450/depositphotos_287964942-stock-photo-kiev-ukraine-july-22-2017.jpg"
+    },
+    {
+        "id":"2",       
+        "title": "Nova estreia nos cinemas",
+        "description": "Saiba o que mudou",
+        "photoCover":"https://st4.depositphotos.com/4634235/28796/i/450/depositphotos_287964942-stock-photo-kiev-ukraine-july-22-2017.jpg"
+    },
+    {
+        "id":"3",       
+        "title": "Filme destaca a construção da estrela da morte",
+        "description": "Fique por dentro da sua origem",
+        "photoCover":"https://st3.depositphotos.com/1310390/13936/v/450/depositphotos_139367812-stock-illustration-speed-warp-vector-background-radiator.jpg"
+    },
+    {
+        "id":"4",       
+        "title": "Cheia de garra e determinismo",
+        "description": "Mais uma mulher de grande força",
+        "photoCover":"https://st2.depositphotos.com/6644582/9428/i/450/depositphotos_94284902-stock-photo-rey-and-bb8-from-star.jpg"
+    }
+]
+   
